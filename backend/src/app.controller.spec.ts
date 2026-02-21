3941a656fe2e2363aab8f5a2c2f4182659a2ef06
waeldaagi <daagi.wael@esprit.tn>
2026-02-21 19:22:36 +0100

    test: import ConfigService for AppController spec

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { ApiInfo, AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
